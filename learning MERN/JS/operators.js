23380d10cd50d4f1fc72b289aac52664390b9d29
Megharani Uppin Nagaraju <megharani.n@spanservices.com>
2019-07-04 21:28:47 +0530

    started variable,operators js

---
+++ learning MERN/JS/operators.js
@@ -0,0 +1,53 @@
+// Assignment operator ;
+// Assignment x=y
+// Plus Assignment +=
+// Subtract Assignment -=
+// Multiplication Assignment x*=y
+// Divison Assignment x/=y
+// Remainder Assignment x%=y
+
+var a = 25 ;
+console.log(a);
+
+// Plus Assignment Operator
+
+var zoo = 45 ;
+
+zoo = zoo + 10 ; 
+
+// zoo += 10 ;
+
+console.log(zoo)
+
+// Subtract Assignment Operator
+
+var w = 97 ;
+
+w -= 23;
+
+console.log(w)
+
+// Multiplication Assignment Operator
+
+var aane = 50 ;
+
+aane *= 5;
+
+console.log(aane)
+
+// Divison Assignment Operator
+
+var p = 56 ;
+
+p /= 4 ;
+
+console.log(p)
+
+
+// Remainder Assignment Operator
+
+var r = 45 ;
+
+r %= 12;
+
+console.log(r)
---
+++ learning MERN/JS/variable.js
@@ -0,0 +1,61 @@
+// Declaration
+
+var totalprice ; // Declaration Statement
+
+totalprice = 2000; // assignment
+
+console.log(totalprice);
+
+var discountedprice = 1000;
+ console.log(discountedprice);
+
+ 
+
+
+ var a = 14;
+
+ var A = 98 ;
+
+ console.log(a);
+ 
+ console.log(A);
+
+ var PI = 3.16;
+
+ var _ = 15;
+
+ console.log(_);
+
+ // CamelCasing
+
+ var totalMarksInMath = 45;
+
+ // Underscore
+
+ var total_marks_Kannada = 67;
+
+ var x = 45;
+
+ var y = x ;
+
+ console.log(y);
+
+
+ // var prod ;
+
+ prod2 = 46;
+
+ console.log(prod2);
+
+ // reassignment
+
+ prod2 = "Apple" ;
+
+ console.log(prod2);
+
+
+ var prod = 43 ;
+
+ prod = true ;
+
+ console.log(prod);
